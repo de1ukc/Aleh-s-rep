4cc54b82d39ea49e0c2cc005001718cf1524c99c
Aleh <79203780+de1ukc@users.noreply.github.com>
2021-03-19 11:24:30 +0300

    Update Program.cs

--- LAB_3/LAB_3&5/Program.cs
+++ LAB_3/LAB_3&5/Program.cs
@@ -307,38 +307,45 @@ namespace MyStudents
             //////////////////////////////////////////////////////////////
             Console.WriteLine("Enter your firstname : ");
             me.firstname = Console.ReadLine();
-            while (Word(me.firstname))
+            while (Word(me.firstname) || me.firstname == "")
             { Console.WriteLine("You are liar.Enter your firstname correctly : ");
                 me.firstname = Console.ReadLine();
             }
             //////////////////////////////////////////////////////////////
             Console.WriteLine("Enter your secondname : ");
             me.secondname = Console.ReadLine();
-            while (Word(me.secondname))
+            while (Word(me.secondname) || me.secondname == "")
             { Console.WriteLine("You are liar.Enter your secondname correctly : ");
                 me.secondname = Console.ReadLine();
             }
             ////////////////////////////////////////////////////////////////
             Console.WriteLine("Enter your lastname : ");
             me.lastname = Console.ReadLine();
-            while (Word(me.lastname))
+            while (Word(me.lastname) || me.lastname == "")
             { Console.WriteLine("You are liar.Enter your lastname correctly : ");
                 me.lastname = Console.ReadLine();
             }
             ////////////////////////////////////////////////////////////////
             Console.WriteLine("Enter your Age : ");
-            me.age = Int32.Parse(Number(Console.ReadLine()));
-            while (me.age >= 120 || me.age < 17)
+            bool testage = Int32.TryParse(Console.ReadLine(),out me.age);
+            //me.age = Int32.Parse(Number(Console.ReadLine()));
+            while (me.age >= 120 || me.age < 17 || testage == false )
             { Console.WriteLine("I know that you've lide. Please enter your age :");
-                me.age = Int32.Parse(Number(Console.ReadLine()));
+                //me.age = Int32.Parse(Number(Console.ReadLine()));
+                 testage = Int32.TryParse(Console.ReadLine(), out me.age);
             }
             //////////////////////////////////////////////////////////////////
             Console.WriteLine("Enter your group:");
             me.group = Console.ReadLine();
+            while (me.@group == "")
+            {Console.WriteLine("Please, write your group correctly:");
+                me.group = Console.ReadLine();
+            }
+
             //////////////////////////////////////////////////////////////////
             Console.WriteLine("Enter your spezialisation:");
             me.Spezialisation= Console.ReadLine();
-            while (Word(me.Spezialisation))
+            while (Word(me.Spezialisation) || me.Spezialisation == "")
             { Console.WriteLine("You are liar.Enter your spezialisation correctly : ");
                 me.Spezialisation = Console.ReadLine();
             }
