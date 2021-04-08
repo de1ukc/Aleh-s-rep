2d6dff0ab3bfe56a671efb85cc437e129c0762b8
Aleh <Trapper.MD@mail.ru>
2021-04-08 22:06:34 +0300

    same lab 6 but with normal files

---
+++ Lab_6_raznes/Lab_6_raznes/MyEnum.cs
@@ -0,0 +1,18 @@
+﻿namespace Lab_6_raznes
+{
+    public enum MyEnum : int
+    {
+        Sleep = 1,
+        ShortSleep ,
+        GYM ,
+        Doctor ,
+        Dota ,
+        skip_classes ,
+        MMA ,
+        went ,
+        info ,
+        exit = 0,
+        labCreate = 5,
+        labPass = 6
+    }
+}
---
+++ Lab_6_raznes/Lab_6_raznes/Student.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Lab_6_raznes
+{
+    public class Student : Human
+    {
+        protected int knowledge;// max 100 
+        protected static int[] retaking = new int[10] ; // количество пересдач у всех студентов
+
+        public static void Retek(Studentofspezialisation a)
+        {
+            if (a.knowledge < 20)
+            { 
+                Console.WriteLine("you've missed to much lectures , you will have retaken");
+                a[0] = 0;
+                a[0]++;
+
+            }
+        }
+        
+        public Student(int HP, int AP, int Power, int knowledge) : base(HP, AP, Power)
+        {
+            this.knowledge = knowledge;
+        }
+        
+        public string group;
+        
+        public void Fuckingaround()
+        {  Console.WriteLine("Well, your a typical student, and it' another day when you fucking around instead of classes , may be you are too bored");
+            this.AP -= 2;
+            if (this.AP <= 0)
+            {
+                this.AP = 0; 
+                Console.WriteLine("Oh, you are to bord, you can't go to walk");
+            }
+            else
+            {
+                this.knowledge -= 4;
+                if (this.knowledge < 0)
+                {
+                    this.knowledge = 0;
+                    Console.WriteLine("I guess that you don't pass the session");
+                }
+            }
+        }
+        
+        public void Info() 
+        { Console.WriteLine($"Name : {firstname}");
+            Console.WriteLine($"Secondname : {secondname}");
+            Console.WriteLine($"Lastname : {lastname}");
+            Console.WriteLine($"Age : {age}");
+            Console.WriteLine($"HP: {HP}");
+            Console.WriteLine($"AP: {AP}");
+            Console.WriteLine($"Power: {power}");
+            Console.WriteLine($"Knowledge: {this.knowledge}");
+            Console.WriteLine($"group : {this.group}");
+        }
+    }
+}
