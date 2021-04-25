c6baa3f5a30fb8b0d6458cb570645d4c760d5e10
Aleh <Trapper.MD@mail.ru>
2021-04-25 14:33:50 +0300

    Rational number

---
+++ LAB_7/Calc/Calc/MyExceptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Calc
+{
+    public class ParseExceptions : Exception
+    {
+        public ParseExceptions(string message) : base(message)
+        {
+        }
+    }
+
+    public class EnteringExceptions : Exception
+    {
+        public EnteringExceptions(string message) : base(message)
+        {
+        }
+    }
+}
