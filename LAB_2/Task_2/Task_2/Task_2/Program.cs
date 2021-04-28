72c0cc7864dd1927bead075c265a78d0eacb4dfb
Aleh <Trapper.MD@mail.ru>
2021-04-28 17:05:20 +0300

    old lab finally commit

---
+++ LAB_2/Task_2/Task_2/Task_2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace Task_2
+{
+    class A
+    {
+        public void cout()
+        {
+            Console.WriteLine("HUI");
+        }
+    }
+
+    class B : A
+    {
+        public void cout()
+        {
+            Console.WriteLine("PIZDA");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        /*
+            string[] bufer;
+            bufer = Console.ReadLine().Split();
+            Array.Reverse(bufer);
+            for (int i = 0; i < bufer.Length; i++)
+            {
+                Console.Write(bufer[i] + " "); 
+            }
+            */
+        B a = new B();
+        a.cout();
+        }
+    }
+}
